50e843ee5b4b5ecf2e71d620a9546fbf82b34060
charlles69 <146854189+charlles69@users.noreply.github.com>
2024-09-13 07:52:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('.mudarTextoBotao').addEventListener('click', function(){
+const paragrafo = document.querySelector('descricao');
+paragrafo.textContent= 'Texto alterado!';
+})
